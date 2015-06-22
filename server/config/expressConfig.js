10af599653657d3c68bc2b97a36c4c4fe8bcf0bb
Amir Rajan <ar@amirrajan.net>
2015-06-22 18:31:48 -0500

    body parser deprecated it seems, updated to use new api

--- server/config/expressConfig.js
+++ server/config/expressConfig.js
@@ -8,7 +8,8 @@ function init(app) {
   app.set('view engine', 'ejs');
   app.use(express.static("public"));
   app.use(express.static("bower_components"));
-  app.use(bodyParser());
+  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.json());
   app.use(cookieParser());
   app.use(cookieSession({
     secret: env.cookieSecret,
